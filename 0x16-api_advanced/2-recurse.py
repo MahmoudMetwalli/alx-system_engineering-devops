a304f226ce4e9080b5a0733a4bb21a10c2670ceb
MahmoudMetwalli <mammkbih@gmail.com>
2024-06-05 02:54:07 +0300

    2024-06-05 02:54:07 AM

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,15 +18,12 @@ def recurse(subreddit, hot_list=[], after=None):
         data = response.json()
         posts = data['data']['children']
 
-        # Add the titles of the posts to the hot_list
         for post in posts:
             hot_list.append(post['data']['title'])
 
-        # Get the 'after' value for the next page of results
         after = data['data']['after']
 
         if after is not None:
-            # Recursive call to get the next page of results
             return recurse(subreddit, hot_list, after)
         else:
             return hot_list
